43023d92226bd6650bd27f64eebc5796345c0080
Ometeor-Zheero-OMZ <sicsempertyrannis98@gmail.com>
2025-07-03 00:59:49 +0900

    feat: ColorType method to check whether or not is float

--- media-core/src/image/formats.rs
+++ media-core/src/image/formats.rs
@@ -150,4 +150,9 @@ impl ColorType {
     pub fn has_alpha(self) -> bool {
         matches!(self, Self::La8 | Self::La16 | Self::Rgba8 | Self::Rgba16 | Self::Rgba32F)
     }
+
+    /// 浮動小数点形式かどうか
+    pub fn is_float(self) -> bool {
+        matches!(self, Self::Rgb32F | Self::Rgba32F)
+    }
 }
