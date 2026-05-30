d01ddbf6560d7c3ccd3a4714d8e3eac1ae5f8f62
itsakeyfut <itsakey0xff@gmail.com>
2026-05-31 01:53:59 +0900

    refactor(downloader): remove dead utils.rs and prune unused deps

--- app/downloader/src/lib.rs
+++ app/downloader/src/lib.rs
@@ -1,8 +1,5 @@
-#![allow(dead_code)]
-
 mod errors;
-mod utils;
 mod youtube;
 
 pub use errors::{DownloaderError, Result};
-pub use youtube::download_video;
+pub use youtube::{FormatInfo, VideoInfo, download_video, get_video_info, list_formats};
